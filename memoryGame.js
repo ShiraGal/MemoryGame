8b41f038a8c9c31ead758efd4b2d17c51b1374ab
shira <shira8sara@gmail.com>
2022-04-28 17:29:26 +0300

    start function

--- memoryGame.js
+++ memoryGame.js
@@ -1,42 +1,63 @@
 
  let cards = ["A","B","C","D","E","F","G","H","I","J"]
-cards = cards.concat(cards);
-
-function shuffle(){
-
-    for(i in cards){
-        randomI = Math.round(Math.random()*(cards.length-1));
-        cards[randomI] = String(cards.splice(i,1,cards[randomI]));
+ cards = cards.concat(cards);
+ let board = document.getElementById("gameTable");
+ let arrOpen = [];
+ let lastCard;
+ 
+ //=============================================================
+ function shuffle(){
+     
+     for(i in cards){
+         randomI = Math.round(Math.random()*(cards.length-1));
+         cards[randomI] = String(cards.splice(i,1,cards[randomI]));
+        }
+        return cards;
     }
-    return cards;
-}
-console.log(shuffle());
+    console.log(shuffle());
+//==============================================================
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-
-
-// למשוך את שולחן המשחק:
-let board = document.getElementById("gameTable");
-// לייצר כרטיס:
-let elem = document.createElement("div");
-// להכניס טקסט בכרטיס
-// elem.innerText = "A";
-// לחבר את הכרטיס לשולחן, את הילד לאבא..
-board.appendChild(elem);
-
-// elem.className = "card";
-
-
-
-
-function startNewGame(){
+function startGame(){
     for(i in cards){
         let elem = document.createElement("div");
-        elem.innerText = cards[i];
-        board.appendChild(elem);
         elem.className = "card";
+        elem.id = cards[i];
+        
+        elem.onclick = (e) => {
+            let openCard = e.target;
+            e.target.innerHTML = elem.id;
+            openCard.style.visibility = "visible";
+            
+            arrOpen.push(openCard.innerHTML);
+            console.log(arrOpen);
+
+            if(arrOpen.length == 2){
+
+                if(arrOpen[0] == arrOpen[1]){
+                    
+                    alert("good!");
+                    arrOpen = [];
+                    console.log(arrOpen);
+                   
+                    openCard.style.visibility = 'hidden'
+                    lastCard.style.visibility = 'hidden' 
+                }
+                else{
+                    arrOpen = [];
+                    openCard.innerHTML = "";
+                    lastCard.innerHTML = "";
+                }
+            }
+            else{
+                lastCard = openCard;
+            }
+        }
+
+        board.appendChild(elem);
     }
     return cards
 }
-startNewGame();
+startGame();
 
